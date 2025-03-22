176c38cbb69a934f9f0912bb05eab163fdf767a4
ynfleozin <leonardoalvarengam@gmail.com>
2025-03-22 18:39:22 -0300

    refactor: adiciona tratamento de erros

--- src/services/productsScraper.ts
+++ src/services/productsScraper.ts
@@ -1,38 +1,51 @@
 import puppeteer from "puppeteer";
 
 export async function getProducts() {
-  const url = "https://www.amazon.com.br/gp/bestsellers/";
+  let browser;
+  try {
+    const url = "https://www.amazon.com.br/gp/bestsellers/";
 
-  const browser = await puppeteer.launch();
+    browser = await puppeteer.launch();
 
-  const page = await browser.newPage();
-  await page.goto(url);
+    const page = await browser.newPage();
+    await page.goto(url);
 
-  const products = await page.evaluate(() => {
-    const content = document.querySelectorAll(".p13n-sc-uncoverable-faceout");
+    await page.waitForSelector(".p13n-sc-uncoverable-faceout");
+    const products = await page.evaluate(() => {
+      const content = document.querySelectorAll(".p13n-sc-uncoverable-faceout");
 
-    return Array.from(content)
-      .slice(0, 3)
-      .map((item) => {
-        const title = item
-          .querySelector(".p13n-sc-truncated")
-          ?.textContent?.trim();
-        const price = item
-          .querySelector("._cDEzb_p13n-sc-price_3mJ9Z")
-          ?.textContent?.trim();
+      return Array.from(content)
+        .slice(0, 3)
+        .map((item) => {
+          const title = item
+            .querySelector(".p13n-sc-truncated")
+            ?.textContent?.trim();
+          const price = item
+            .querySelector("._cDEzb_p13n-sc-price_3mJ9Z")
+            ?.textContent?.trim();
 
-        return {
-          title,
-          price,
-        };
-      });
-  });
-  
-  await browser.close();
+          return {
+            title,
+            price,
+          };
+        });
+    });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ products }),
-    headers: { "Content-Type": "application/json" },
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ products }),
+      headers: { "Content-Type": "application/json" },
+    };
+  } catch (error) {
+    console.error("Error:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal Server Error" }),
+      headers: { "Content-Type": "application/json" },
+    };
+  } finally {
+    if(browser) {
+        await browser.close();
+    }
+  }
 }
